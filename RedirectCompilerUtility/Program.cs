0e06943d12783c7a231c1d95301c5afda649d735
jpishimwe <jeanpaulishimwe@gmail.com>
2023-02-21 16:22:45 +0100

    initial commit

--- RedirectCompilerUtility/Program.cs
+++ RedirectCompilerUtility/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace RedirectCompilerUtility
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		static void LaunchUtility()
+		{
+
+		}
+	}
+}
+
